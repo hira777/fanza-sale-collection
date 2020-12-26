9ca18126efebfd2bd0d1954aaafb722ca9225cd9
soarflat <soarflat777@gmail.com>
2020-12-26 23:29:31 +0900

    feat: DMM API に関連する型を追加

--- pages/api/ItemList.ts
+++ pages/api/ItemList.ts
@@ -1,18 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import axios from 'axios';
+import { ItemListRequestParameters, ItemListResponse } from '../../types/api';
+
+const params: ItemListRequestParameters = {
+  api_id: process.env.API_ID,
+  affiliate_id: process.env.AFFILIATE_ID,
+  site: 'FANZA',
+  service: 'digital',
+  floor: 'videoa',
+  hits: 100,
+  output: 'json',
+};
 
 const client = axios.create({
   baseURL: 'https://api.dmm.com/affiliate/v3/',
-  params: {
-    api_id: process.env.API_ID,
-    affiliate_id: process.env.AFFILIATE_ID,
-    site: 'FANZA',
-    service: 'digital',
-    floor: 'videoa',
-    hits: 100,
-    output: 'json',
-  },
+  params: params,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -21,7 +24,7 @@ const client = axios.create({
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const response = await client.get('/ItemList');
+    const response = await client.get<ItemListResponse>('/ItemList');
     res.json(response.data.result);
   } catch (error) {
     console.error(error);
