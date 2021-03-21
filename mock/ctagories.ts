2bfcc3cf3c1a389820817de573b735da7203ccea
soarflat <soarflat777@gmail.com>
2021-03-21 18:55:25 +0900

    chore: カテゴリのモックを追加

---
+++ mock/ctagories.ts
@@ -0,0 +1,11 @@
+export const getCategories = () => {
+  return [
+    '春のパンツまつり30％OFF第2弾',
+    'SODクリエイト30％OFF',
+    'プレステージ30％OFF',
+    '妄想族Grp作品30％OFF',
+    'VENUS・熟女JAPAN他30％OFF',
+    'ブランドストア30％OFF☆',
+    '期間限定セール',
+  ];
+};
