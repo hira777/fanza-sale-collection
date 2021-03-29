6c595fd3f1242046d93e1267a7d859c46da3f850
soarflat <soarflat777@gmail.com>
2021-03-29 17:38:40 +0900

    feat: ページネーションのコンポーネントを追加

---
+++ components/Pagination.tsx
@@ -0,0 +1,28 @@
+import { useState, ChangeEvent } from 'react';
+import { default as MaterialPagination } from '@material-ui/lab/Pagination';
+
+export type PaginationProps = {
+  page: number;
+  count: number;
+  onChange: (value: number) => void;
+};
+
+export function Pagination({ page: initialPage, count, onChange: onChangePage }: PaginationProps) {
+  const [page, setPage] = useState(initialPage);
+  const onChange = (event: ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+    onChangePage(value);
+  };
+
+  return (
+    <div>
+      <MaterialPagination
+        variant="outlined"
+        shape="rounded"
+        count={count}
+        page={page}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
