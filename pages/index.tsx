55dda9aa70588459ee9f52d8a65876318e23c6e5
soarflat <soarflat777@gmail.com>
2021-04-01 15:29:15 +0900

    refactor: Top コンポーネントを削除
    現状の作りだと Next.js の Page（pages/index.ts） から、画面を表示するための
    Top コンポーネントに Props を渡し、レンダリングしていたが、わざわざ Top コンポーネントを
    介する必要がないと判断したので、pages/index.ts でレンダリングもするようにした。
    
    そもそも、なぜ Top コンポーネントが存在したのか？
    Story を追加する時に、pages/index.ts を Story にすると Props を渡すのが大変だったから
    Props を受け取ってレンダリングするだけの Top コンポーネントを追加した記憶がある。
    （コンポーネント内で process.env.NEXT_PUBLIC_xxx の値を受け取るのがイケてないと思った気がするがあまり覚えていないい）
    とりあえず、現時点では Top コンポーネントは不要だと判断したので削除。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,22 @@
+import { useMemo } from 'react';
 import { GetServerSideProps } from 'next';
 import Head from 'next/head';
+import Container from '@material-ui/core/Container';
+
 import { itemListService } from '../services/itemList';
 import { ItemListResponseResultField } from '../types/api/';
 import { useItemList } from '../hooks/useItemList';
-import { Top } from '../screens/Top';
+import { ItemList } from '../components/ItemList';
+import { ItemListItem } from '../components/ItemListItem';
+import { ResultStats } from '../components/ResultStats';
 import { Header } from '../components/Header';
 import { CATEGORIES } from '../constants/categoriesOfSearch';
 
-export default function Home({
-  initialResponse,
-}: {
+export type HomeProps = {
   initialResponse: ItemListResponseResultField;
-}) {
+};
+
+export default function Home({ initialResponse }: HomeProps) {
   const { response, keyword, setCategory, setInputValue } = useItemList({
     response: initialResponse,
     category: CATEGORIES[0],
@@ -22,6 +27,16 @@ export default function Home({
   const onChangeCategory = (selectedCategory: string) => {
     setCategory(selectedCategory);
   };
+  const itemList = useMemo(
+    () => (
+      <ItemList>
+        {response.items.map((item) => (
+          <ItemListItem key={item.product_id} item={item} />
+        ))}
+      </ItemList>
+    ),
+    [response.items]
+  );
 
   return (
     <div>
@@ -29,13 +44,13 @@ export default function Home({
         <title>Fanza Sale Collection</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Top
-        response={response}
-        keyword={keyword}
-        header={
-          <Header categories={CATEGORIES} onChangeCategory={onChangeCategory} onSubmit={onSubmit} />
-        }
-      />
+      <Header categories={CATEGORIES} onChangeCategory={onChangeCategory} onSubmit={onSubmit} />
+      <main style={{ marginTop: 20 }}>
+        <Container fixed maxWidth="md">
+          <ResultStats keyword={keyword} response={response} />
+          <div style={{ marginTop: 10 }}>{itemList}</div>
+        </Container>
+      </main>
     </div>
   );
 }
