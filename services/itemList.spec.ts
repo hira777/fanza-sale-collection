f148d61708bf47a427d9122bca749867fded6e65
soarflat <soarflat777@gmail.com>
2021-04-20 22:51:19 +0900

    refactor: itemList service で利用する baseURL を環境変数から取得するようにした。
    また、itemList service のテストが、本来テストをしたいこと以外の無駄な箇所もテストしていると感じたので修正した。

--- services/itemList.spec.ts
+++ services/itemList.spec.ts
@@ -1,37 +1,28 @@
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { itemListService } from './itemList';
-import { getItems } from '../mocks/items';
-import { ItemListResponseResult as Response } from '../types/api';
 
-const items = getItems();
-const response: Response = {
-  status: 200,
-  result_count: 100,
-  total_count: 1000,
-  first_position: 1,
-  items: items,
-};
 const server = setupServer(
   ...[
-    rest.get('http://localhost:3000/api/ItemList', (req, res, ctx) => {
+    rest.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}api/ItemList`, (req, res, ctx) => {
       const query = req.url.searchParams;
       const keyword = query.get('keyword');
       const offset = query.get('offset');
 
-      if (keyword || offset) {
-        return res(
-          ctx.status(200),
-          ctx.json({
-            ...response,
-            ...{
-              items: [keyword ? items[0] : items[1]],
-            },
-          })
-        );
+      if (keyword) {
+        return res(ctx.status(200), ctx.json({ id: 2 }));
       }
 
-      return res(ctx.status(200), ctx.json(response));
+      if (offset) {
+        return res(ctx.status(200), ctx.json({ id: 3 }));
+      }
+
+      // itemListService のテストの目的は itemListService が、API の正しいエンドポイントにリクエストして、
+      // レスポンスを取得できるかどうかなので、レスポンスの内容自体はなんでも良いと思っている。
+      // itemListService のテストで、API が本来返却するものを取得しているかまでをテストするのは、
+      // itemListService のテストからは逸脱している気がしており、そのテストは API 自体のテストで行うべきだと思っている。
+      // そのため、適当なレスポンスを返却するように指定している。
+      return res(ctx.status(200), ctx.json({ id: 1 }));
     }),
   ]
 );
@@ -46,31 +37,32 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe(`itemList service`, () => {
-  it('API にリクエストしてレスポンスを取得する', async () => {
-    const result = await itemListService.get();
+  describe(`get`, () => {
+    it('API の正しいエンドポイントにリクエストする', async () => {
+      const result = await itemListService.get();
 
-    expect(result.data).toEqual(response);
-  });
-
-  it('keyword パラメータを付与して API にリクエストしてレスポンスを取得する', async () => {
-    const result = await itemListService.get({ keyword: 'セール' });
-
-    expect(result.data).toEqual({
-      ...response,
-      ...{
-        items: [items[0]],
-      },
+      expect(result.status).toBe(200);
     });
-  });
 
-  it('offset パラメータを API にリクエストしてレスポンスを取得する', async () => {
-    const result = await itemListService.get({ offset: 1 });
+    it('API にリクエストしてレスポンスを取得する', async () => {
+      const result = await itemListService.get();
 
-    expect(result.data).toEqual({
-      ...response,
-      ...{
-        items: [items[1]],
-      },
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual({ id: 1 });
+    });
+
+    it('keyword クエリパラメータを付与して API にリクエストし、レスポンスを取得する', async () => {
+      const result = await itemListService.get({ keyword: 'セール' });
+
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual({ id: 2 });
+    });
+
+    it('offset クエリパラメータを付与して API にリクエストし、レスポンスを取得する', async () => {
+      const result = await itemListService.get({ offset: 1 });
+
+      expect(result.status).toBe(200);
+      expect(result.data).toEqual({ id: 3 });
     });
   });
 });
